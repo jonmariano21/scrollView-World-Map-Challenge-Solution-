dd314e663f57e2073a9ee2a001e6f74c0b8923c1
jonmariano21 <jonmariano21@gmail.com>
2014-06-20 18:02:24 -0700

    Functioning scrollView

--- World Map Challenge Solution/JMViewController.h
+++ World Map Challenge Solution/JMViewController.h
@@ -8,6 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
-@interface JMViewController : UIViewController
+@interface JMViewController : UIViewController <UIScrollViewDelegate>//Protocol: now have access to scroll view delegate methods
+
+@property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
+
+//Property for image that will be ON TOP of scrollView
+@property (strong, nonatomic) UIImageView *globalImageView;
 
 @end
--- World Map Challenge Solution/JMViewController.m
+++ World Map Challenge Solution/JMViewController.m
@@ -18,6 +18,23 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    //Assign image property an image
+    self.globalImageView = [ [UIImageView alloc] initWithImage:[UIImage imageNamed:@"worldMap.jpeg"] ];
+    
+    //Be able to scroll over entire image
+    self.scrollView.contentSize = self.globalImageView.frame.size;
+    
+    //Add image view OM TOP of scrollView
+    [ self.scrollView addSubview:self.globalImageView];
+    
+    //Set up delegate property for the scrollView
+    // Let the view controller "JMViewController.m" become the delegate of our scrollView
+    self.scrollView.delegate = self;
+    
+    self.scrollView.maximumZoomScale = 2.0;
+    self.scrollView.minimumZoomScale = 0.5;
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +42,13 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+-(UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView{
+    
+    return self.globalImageView;
+    
+}
+
 
 @end
